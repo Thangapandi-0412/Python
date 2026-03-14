ef3d918fb305caa3a1fa07b96156d93b5a67fdb5
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:22:41 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=a+b
+print("the addition of two number is",c) 
+d=(a+b)/2
+print("the average of the number is",d)
+e=c-d
+print("The deviation is",e)
+
