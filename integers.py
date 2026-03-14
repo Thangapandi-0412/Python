2a9833c6f126a56546f07584b2533e58caf34975
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:24:06 +0530

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,11 @@
+list1=list(int(input("enter the integer list:")))
+evenlist=[ ]
+oddlist=[ ]
+for i in list:
+   if (i%2==0):
+      evenlist.append(i)
+   else:
+      oddlist.append(i)
+print("the odd no is:",oddlist)
+print("the evn no is:",evenlist)
+
