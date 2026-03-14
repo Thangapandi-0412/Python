e2c9012e842d37fc6fe8abbc033f26b989b7871b
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:25:55 +0530

    Add files via upload

---
+++ result.py
@@ -0,0 +1,17 @@
+a1=int(input("enter the first exam mark"))
+a2=int(input("enter the second exam mark"))
+a3=(a1+a2)/2
+a=a3*0.5
+print("the precentage of mark is",a)
+b1=int(input("enter the sport mark"))
+b=(b1)*0.2
+print("the precentage of sports mark is",b)
+c1=int(input("enter the first activity"))
+c2=int(input("enter the second activity"))
+c3=int(input("enter the third activity"))
+c4=(c1+c2+c3)/3
+c=c4*0.3
+print("the precentage of activity mark",c)
+d=a+b+c
+print("the total precentage of student is",d)
+
