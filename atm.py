30aa3a691303ac6b5743b00fc659ad07d26a1e27
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:22:27 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,16 @@
+x=int(input("Enter the bill amount"))
+y=int(input("enter the reciving amount"))
+a=y-x
+print("the remaining amount",a)
+a1=a//500
+print("500x",a1)
+b=a%500
+b1=b//200
+print("200x",b1)
+c=b%200
+c1=c//100
+print("100x",c1)
+d=c%100
+d1=d//50
+print("50x",d1)
+
