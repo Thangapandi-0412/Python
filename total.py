98f33f2bc9e2f5949b93d24caaac66b986e1c313
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:27:56 +0530

    Add files via upload

---
+++ total.py
@@ -0,0 +1,13 @@
+def total(a,b,c,d,e):
+   return a+b+c+d+e
+def average(x):
+      return (x)/5
+a=int(input("Enter the number mark 1:")
+b=int(input("Enter the number mark 2:")
+c=int(input("Enter the number mark 3:")
+d=int(input("Enter the number mark 4:")
+e=int(input("Enter the number mark 5:")
+result total = total(o,p,q,r,s)
+result average = average (result total)
+print("The total mrak is",result total)
+print("The average is",result average)
