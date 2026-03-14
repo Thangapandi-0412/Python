3c1f0ae41173701695be6b61b3a4e89ddc52d7ce
Thangapandi-0412 <ponnusamythangam2005@gmail.com>
2026-03-14 12:23:55 +0530

    Add files via upload

---
+++ equal.py
@@ -0,0 +1,9 @@
+def num(a,b):
+   return a == b
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+result = num
+if a == b:
+   print("Both are equal")
+else:
+   print("Both is not equal")
